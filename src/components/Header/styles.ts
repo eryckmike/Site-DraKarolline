1b34f9a5df1ce4b704ec6e766b1a350eebdc199b
Eryck Mike <eryckmike@MacBook-Pro-de-Eryck.local>
2025-07-24 21:53:46 -0300

    att

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -29,7 +29,7 @@ export const Logo = styled.img`
 
 export const ButtonGroup = styled.div`
   position: absolute;
-  left: 46%;
+  left: 50%;
   transform: translateX(-50%);
   display: flex;
   gap: 2rem;
